522c9ee81bda5b138f9d895dd1ffdecf4e0d8633
Krisztián <Krisztian@pometko.hu>
2020-10-12 10:49:07 +0200

    Added model for storing references to seperate log entities

---
+++ EliteBroker/Models/DataStore.cs
@@ -0,0 +1,12 @@
+﻿using EliteBroker.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EliteBroker
+{
+    public class DataStore
+    {
+        public MarketData Market { get; set; }
+    }
+}
