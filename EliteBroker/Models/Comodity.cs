de079b7acf9ad19732cf7dd98a3979c0c6b7b12b
Krisztián <Krisztian@pometko.hu>
2020-10-12 10:44:36 +0200

    Added models for market data and comodities

---
+++ EliteBroker/Models/Comodity.cs
@@ -0,0 +1,25 @@
+﻿using PropertyChanged;
+
+namespace EliteBroker.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Comodity
+    {
+        private int buyPrice;
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Name_Localised { get; set; }
+        public string Category { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public int MeanPrice { get; set; }
+        public int StockBraket { get; set; }
+        public int DemandBracket { get; set; }
+        public int Stock { get; set; }
+        public int Demand { get; set; }
+        public bool Consumer { get; set; }
+        public bool Producer { get; set; }
+        public bool Rare { get; set; }
+    }
+}
---
+++ EliteBroker/Models/MarketData.cs
@@ -0,0 +1,27 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EliteBroker.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MarketData
+    {
+        private ObservableCollection<Comodity> items;
+
+        public MarketData()
+        {
+            Items = new ObservableCollection<Comodity>();
+        }
+
+        public DateTime Date { get; set; }
+        public int MarektID { get; set; }
+        public string StationName { get; set; }
+        public string StationType { get; set; }
+        public string StarSystem { get; set; }
+        public ObservableCollection<Comodity> Items { get; set; }
+        
+    }
+}
