c2c1efc00bd7390b5b576c41decd8ccbd9ca6996
Krisztián <Krisztian@pometko.hu>
2020-10-12 09:58:29 +0200

    Added simple log directory locator

---
+++ EliteBroker/DataConsumers/LogPathLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace EliteBroker.DataConsumers
+{
+    public class LogPathLocator
+    {
+        public string LogPath { get; private set; }
+
+        public LogPathLocator(){
+            var userDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            LogPath = userDirectory + @"Saved Games\Frontier Developments\Elite Dangerous\";
+        }
+    }
+}
