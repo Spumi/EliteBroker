a07f1cee7baa6de9ed7dd6735a41743f1f843a53
Krisztián <Krisztian@pometko.hu>
2020-10-13 10:10:49 +0200

    Implemented notify prop changed for datastore

--- EliteBroker/Models/DataStore.cs
+++ EliteBroker/Models/DataStore.cs
@@ -1,12 +1,19 @@
 ﻿using EliteBroker.Models;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EliteBroker
 {
+    [AddINotifyPropertyChangedInterface]
     public class DataStore
     {
+        public DataStore()
+        {
+            Market = new MarketData();
+        }
+
         public MarketData Market { get; set; }
     }
 }
