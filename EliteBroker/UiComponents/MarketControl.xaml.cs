5c7b14c0d869acce56e7075ea07fb2782f3312af
Krisztián <Krisztian@pometko.hu>
2020-10-13 09:56:56 +0200

    Moved comodities panel to a  tab control

--- EliteBroker/UiComponents/MarketControl.xaml.cs
+++ EliteBroker/UiComponents/MarketControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -22,5 +23,6 @@ namespace EliteBroker.UiComponents
         {
             InitializeComponent();
         }
+
     }
 }
